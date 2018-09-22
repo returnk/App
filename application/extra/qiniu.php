b8efa77b478822d7d25932dee831ff835ece013b
returnk <eshuo@outlook.com>
2018-09-22 22:08:48 +0800

    去掉号码

--- application/extra/qiniu.php
+++ application/extra/qiniu.php
@@ -7,8 +7,8 @@
  */
 
 return [
-    'ak'    =>  'xcxw6968Y1_sN8t8JY394ejGBxQdpMiF_ulfRLxe',
-    'sk'    =>  'xHerrU--YAuZHnCjL5RbOgYny16qU6WwBB61IAdG',
-    'bucket'=>  'appp',
-    'image_url'=> 'http://pdybeh4lh.bkt.clouddn.com',
+    'ak'    =>  'xcxw6968Y1_sN8t8JY394ejGBxQdpMiF_******',
+    'sk'    =>  'xHerrU--YAuZHnCjL5RbOgYny16qU6WwB******',
+    'bucket'=>  'app',
+    'image_url'=> 'http://.bkt.clouddn.com',
 ];
